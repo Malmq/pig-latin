d525cd006c353ad6a064e767bf43d9e41743d737
Brittany Gollins and Marie Almquist <marie@>
2018-10-01 19:46:47 -0700

    take user input and display to user after submit button is engaged

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,9 @@
 // user interface logic
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
+    var word = $("input#word").val();
     event.preventDefault();
 
-
+    $(".word").text(word);
   });
 });
