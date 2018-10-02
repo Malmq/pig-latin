03df15dc1223b8cb27bb55c985e16152932f2f55
Brittany Gollins and Marie Almquist <brittany@>
2018-10-01 20:29:26 -0700

    add javascript to split the user phrase input then push into a new empty array... then display to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 // business logic
 
-
 // user interface logic
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
-    var word = $("input#word").val();
     event.preventDefault();
+    var phrase = $("input#word").val();
+    var words = [];
+    var breakUps = phrase.split(" ");
+//    console.log(breakUps);
+    breakUps.forEach(function(breakUp) {
+      words.push(breakUp);
+    });
+    console.log(words);
+    //word2.push(word3);
 
-    $(".word").text(word);
+    $(".word").text(words);
     $("#result").show();
   });
 });
